7434d3088f4d6c313f472c749f7c15f1dc7829c7
wslmacieira <wslmacieira@outlook.com>
2019-10-23 22:50:55 -0300

    criado agendamento de serviços

--- src/database/migrations/20191024010128-appointments.js
+++ src/database/migrations/20191024010128-appointments.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('appointements', {
+    return queryInterface.createTable('appointments', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -41,6 +41,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('appointements');
+    return queryInterface.dropTable('appointments');
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileControllers';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -18,7 +19,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
