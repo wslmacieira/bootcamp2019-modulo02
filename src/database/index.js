a9396867961050d126ea12764421625c823360f3
wslmacieira <wslmacieira@outlook.com>
2019-10-25 19:45:21 -0300

    criando Notifications para os agendamentos

--- src/database/index.js
+++ src/database/index.js
@@ -25,7 +25,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/dbgobarber',
+      'mongodb://localhost:27017/gobarber',
       { useNewUrlParser: true, useFindAndModify: true }
     );
   }
