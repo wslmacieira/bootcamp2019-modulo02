c7de13d332c23c53771b00f71c763fa70228a967
wslmacieira <wslmacieira@outlook.com>
2019-10-25 20:30:23 -0300

    NotificationController criado método update

--- src/routes.js
+++ src/routes.js
@@ -32,5 +32,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
